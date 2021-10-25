eef0408c01579a8da07610549c3a88912a6ecf9e
Mmalvesada <mariamalvesadarodriguez@hotmail.es>
2021-10-25 23:26:24 +0200

    versión 1
    Trabajo con jugadores como hilos

--- pingpong/src/pingpong/Game.java
+++ pingpong/src/pingpong/Game.java
@@ -6,9 +6,9 @@ package pingpong;
 
 /**
  *
- * @author 34626
+ * @author Maria Malvesada
  */
-public class Game {
+/*public class Game {
 
 
 
@@ -44,4 +44,106 @@ public class Game {
 
     }
 
+}*/
+
+/*public class Game {
+
+
+
+    public static final int MAX_TURNS = 10;
+
+
+
+    public static void main(String[] args) {
+
+
+
+        Player player1 = new Player("ping");
+
+        Player player2 = new Player("pong");
+
+
+
+        player1.setNextPlayer(player2);
+
+        player2.setNextPlayer(player1);
+
+
+
+        System.out.println("Game starting...!");
+
+
+
+        player1.setMustPlay(true);
+
+
+
+        Thread thread2 = new Thread(player2);
+
+        thread2.start();
+
+        Thread thread1 = new Thread(player1);
+
+        thread1.start();
+
+
+
+        System.out.println("Game finished!");
+
+    }
+
+
+
 }
+*/
+
+public class Game {
+
+
+
+    public static final int MAX_TURNS = 10;
+
+
+
+    public static void main(String[] args) {
+
+
+
+        Player player1 = new Player("ping");
+
+        Player player2 = new Player("pong");
+
+
+
+        player1.setNextPlayer(player2);
+
+        player2.setNextPlayer(player1);
+
+
+
+        System.out.println("Game starting...!");
+
+
+
+        player1.setMustPlay(true);
+
+
+
+        Thread thread2 = new Thread(player2);
+
+        thread2.start();
+
+        Thread thread1 = new Thread(player1);
+
+        thread1.start();
+
+
+
+        System.out.println("Game finished!");
+
+    }
+
+
+
+}
+
--- pingpong/src/pingpong/Player.java
+++ pingpong/src/pingpong/Player.java
@@ -6,9 +6,9 @@ package pingpong;
 
 /**
  *
- * @author 34626
+ * @author María Malvesada
  */
-public class Player {
+/*public class Player {
 
 
 
@@ -63,5 +63,84 @@ public class Player {
     }
 
 
+
+}*/
+
+public class Player implements Runnable {
+
+
+
+    private final String text;
+
+
+
+    private int turns = Game.MAX_TURNS;
+
+
+
+    private Player nextPlayer;
+
+
+
+    private boolean mustPlay = false;
+
+
+
+    public Player(String text) {
+
+        this.text = text;
+
+    }
+
+
+
+    @Override
+
+    public void run() {
+
+        while(!gameFinished()) {
+
+            while(!mustPlay);
+
+
+            System.out.println(text);
+
+            turns--;
+
+
+
+            this.mustPlay = false;
+
+            nextPlayer.mustPlay = true;
+
+
+
+        }
+
+    }
+
+
+
+    private boolean gameFinished() {
+
+        return turns == 0;
+
+    }
+
+
+
+    public void setNextPlayer(Player nextPlayer) {
+
+        this.nextPlayer = nextPlayer;
+
+    }
+
+
+
+    public void setMustPlay(boolean mustPlay) {
+
+        this.mustPlay = mustPlay;
+
+    }
 
 }
