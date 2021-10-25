2877730384db2c2f67f30455a754feb1763a0262
Mmalvesada <mariamalvesadarodriguez@hotmail.es>
2021-10-25 23:34:17 +0200

    vVersión 1b
    cuidado que el código se bloquea porque estamos usando boolean

--- pingpong/src/pingpong/Game.java
+++ pingpong/src/pingpong/Game.java
@@ -97,7 +97,7 @@ package pingpong;
 }
 */
 
-public class Game {
+/*public class Game {
 
 
 
@@ -146,4 +146,70 @@ public class Game {
 
 
 }
+*/
+/*los hilos terminan controlado aunue se queda pillado y no termina
+*hay que reconfigura el codigo 
+*/
+public class Game {
 
+
+
+    public static final int MAX_TURNS = 10;
+
+
+
+    public static void main(String[] args) {
+
+
+
+        Player player1 = new Player("ping");
+
+        Player player2 = new Player("pong");
+
+
+
+        player1.setNextPlayer(player2);
+
+        player2.setNextPlayer(player1);
+
+
+
+        System.out.println("Game starting...!");
+
+
+
+        player1.setMustPlay(true); 
+
+
+
+        Thread thread2 = new Thread(player2);
+
+        thread2.start();
+
+        Thread thread1 = new Thread(player1);
+
+        thread1.start();
+
+
+
+        try {
+
+            thread1.join();
+
+            thread2.join();
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        }
+
+
+
+        System.out.println("Game finished!");
+
+    }
+
+
+
+}
